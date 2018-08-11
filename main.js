720642a421a289650988543db92df4d007914806
ArtikynPlayground <40923089+ArtikynPlayground@users.noreply.github.com>
2018-08-11 21:09:42 +0300

    29 Commit
    Shark showcase

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
-function loveyou(){
-    alert("Love you, Marvik~")
+function onshark(){
+   var shark;
+    shark = document.getElementById("hereShark");
+    shark.outerHTML = '<img src="img/shark.jpg" alt="" id="sharkImg">';
 }
