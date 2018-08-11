89c0e4d55bdb913ede305ce835243958c895123e
ArtikynPlayground <40923089+ArtikynPlayground@users.noreply.github.com>
2018-08-11 23:05:43 +0300

    32 Commit
    Fixed ids madness
    Working on error pass

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-var shark,noshark, password;
-function onshark(){
-    shark = document.getElementById("hereShark");
-    shark.outerHTML = '<div id="sharkWrapper"><img src="img/shark.jpg" alt="" id="sharkImg"><p>Введите имя создателя чтобы убрать акулку и нажмите на кнопку: <input type="text" id="password"><button onclick="checkPass()">Убрать</button></p></div>';
+var placeForShark,noshark,wrong, error, password;
+function showShark(){
+    placeForShark = document.getElementById("placeForShark");
+    placeForShark.outerHTML = '<div id="sharkWrapper"> <img src="img/shark.jpg" alt="" id="sharkImg"><p>Введите имя создателя чтобы убрать акулку и нажмите на кнопку: <input type="text" id="password"><button onclick="checkPass()">Убрать</button></p></div>';
   }
 function checkPass(){
     noshark = document.getElementById("sharkWrapper");
       password = document.getElementById("password").value
     if (password == "Артур"){
-       noshark.outerHTML = '<a href="#" id="hereShark">#</a>'; 
+       noshark.outerHTML = '<a href="#" id="hereShark">#</a>';
        
     }
-}
+      
+        
+    }
