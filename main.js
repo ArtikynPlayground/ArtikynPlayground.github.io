3fceb90d7322cf4e77dbc47d40e03dff811953f5
ArtikynPlayground <40923089+ArtikynPlayground@users.noreply.github.com>
2018-08-11 21:59:47 +0300

    31 Commit
    Added form for shark

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
+var shark,noshark, password;
 function onshark(){
-   var shark;
     shark = document.getElementById("hereShark");
-    shark.outerHTML = '<img src="img/shark.jpg" alt="" id="sharkImg">';
+    shark.outerHTML = '<div id="sharkWrapper"><img src="img/shark.jpg" alt="" id="sharkImg"><p>Введите имя создателя чтобы убрать акулку и нажмите на кнопку: <input type="text" id="password"><button onclick="checkPass()">Убрать</button></p></div>';
+  }
+function checkPass(){
+    noshark = document.getElementById("sharkWrapper");
+      password = document.getElementById("password").value
+    if (password == "Артур"){
+       noshark.outerHTML = '<a href="#" id="hereShark">#</a>'; 
+       
+    }
 }
